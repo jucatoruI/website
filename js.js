6c018d64ff39aac53e3b7a1677fc1f1ab48a0031
jucatoruI <jucatorul.w@gmail.com>
2026-01-04 13:18:21 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+const photo = document.querySelector(".photo");
+
+//doesn't rotate on y yet
+function dragonY(){
+    alert("AHHHH, stop that, I'm gonna throw up");
+}
+
+photo.addEventListener("mousedown", dragonY);
+
+//not working 
+//function enlarge(){}
+//photo.addEventListener("click", );
